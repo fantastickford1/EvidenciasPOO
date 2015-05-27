fa015ee2bc27d3587a2d1f271ffe60cbd80b78c5
Carlos ZRobles <carlitos_zenteno@hotmail.com>
2015-05-27 11:15:27 -0500

    AnalizaFile
    Primera practica para evidencias

---
+++ AnalizaFile.java
@@ -0,0 +1,27 @@
+public class AnalizaFile{
+
+  AnalizaFile(String nombre_archivo){
+  }
+
+  int getLines(){
+
+  }
+
+  String getFirstLine(){
+
+  }
+
+  String getLastLine(){
+
+  }
+
+  void setFileName(String nfile){
+
+  }
+
+  String creditos(){
+
+  }
+
+
+}
